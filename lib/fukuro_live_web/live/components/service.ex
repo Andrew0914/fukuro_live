fe5c1aef5e597740772305b09345f4d2d9522cfe
AndrewG <andrewalangm@gmail.com>
2021-07-07 07:24:21 -0500

    🍿 Create Service Component

--- lib/fukuro_live_web/live/components/service.ex
+++ lib/fukuro_live_web/live/components/service.ex
@@ -1,22 +1,27 @@
-defmodule FukuroLiveWeb.Live.Components.Client do
+defmodule FukuroLiveWeb.Live.Components.Service do
   # uses
   use Surface.LiveComponent
 
-  @doc "The x axis position of the client"
+  @doc "The x axis position of the service"
   prop x, :integer, required: true
-  @doc "The y axis position of the client"
+  @doc "The y axis position of the service"
   prop y, :integer, required: true
-  @doc "The label to identify client component"
+  @doc "The label to identify service component"
   prop label, :string, required: true
-  @doc "Request rate for the client"
-  prop request_rate, :integer, default: 1
+  @doc "Max request capacity for the service"
+  prop max_request_capacity, :integer, default: 10
+  @doc "Concurrency, how many request can handle at a time"
+  prop concurrency, :integer, default: 10
+  @doc "List of related resources"
+  prop resources, :list, default: []
 
   # data
   data label_position, :tuple, default: {0, 0}
   data stats_position, :tuple, default: {0, 0}
+  data request_in_progress, :integer, default: 0
+  data failure_rate, :number, default: 0.0
 
   # methods
-
   def update(assings, socket) do
     {:ok,
      socket
@@ -41,6 +46,10 @@ defmodule FukuroLiveWeb.Live.Components.Client do
     socket |> assign(stats_position: {sx + x, sy + y + 65})
   end
 
+  defp fix_stat_y_postion(stats_position, fix \\ 17) do
+    elem(stats_position, 1) + fix
+  end
+
   def render(assigns) do
     ~F"""
     <g id={@id} >
@@ -48,15 +57,21 @@ defmodule FukuroLiveWeb.Live.Components.Client do
         xlink:href="#component"
         x={@x}
         y={@y}
-        stroke="green"
+        stroke="blue"
         stroke-width="1"
         fill="transparent"
         />
       <text x={elem(@label_position, 0)} y={elem(@label_position, 1)}  font-size="14" font-family="Arial">
         {@label}
       </text>
-      <text x={elem(@stats_position, 0)} y={elem(@stats_position, 1)}  font-size="12" font-family="Arial">
-        Request rate: {@request_rate} req/sec
+      <text x={elem(@stats_position, 0)} y={elem(@stats_position, 1)}  font-size="12" font-family="Arial" fill="red">
+        Req. in progress {@request_in_progress}
+      </text>
+      <text x={elem(@stats_position, 0)} y={fix_stat_y_postion(@stats_position)}  font-size="12" font-family="Arial">
+        Max request capacity {@max_request_capacity}
+      </text>
+      <text x={elem(@stats_position, 0)} y={fix_stat_y_postion(@stats_position, 35)}  font-size="12" font-family="Arial" fill="red">
+        Failure Rate {@failure_rate} %
       </text>
     </g>
     """
